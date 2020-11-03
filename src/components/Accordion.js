e5d9f2f84d687280eb18b3e3113cc1e4552cef98
Flo Orford <flo.orford@hotmail.com>
2020-11-03 15:00:04 +0000

    little accordion widget created to demo just the useState hook, midway through the creation of a wikipedia api search comp

---
+++ src/components/Accordion.js
@@ -0,0 +1,29 @@
+import React, { useState } from "react";
+
+const Accordion = ({ items }) => {
+  const [activeIndex, setActiveIndex] = useState(null);
+
+  const onTitleClick = (index) => {
+    setActiveIndex(index);
+  };
+
+  const renderedItems = items.map((item, i) => {
+    const active = i === activeIndex ? "active" : "";
+
+    return (
+      <React.Fragment key={i}>
+        <div onClick={() => onTitleClick(i)} className={`title ${active}`}>
+          <i className='dropdown icon'></i>
+          {item.title}
+        </div>
+        <div className={`content ${active}`}>
+          <p>{item.content}</p>
+        </div>
+      </React.Fragment>
+    );
+  });
+
+  return <div className='ui styled accordion'>{renderedItems}</div>;
+};
+
+export default Accordion;
